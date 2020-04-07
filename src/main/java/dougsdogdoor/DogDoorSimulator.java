75ac0d18f01eccc5ead0a90d4c009ee9beeaa69b
daniedev <a.danielpf21@gmail.com>
2020-04-07 12:20:36 +0530

    Doug's Dog Door With Automatic Close Option

--- src/main/java/dougsdogdoor/DogDoorSimulator.java
+++ src/main/java/dougsdogdoor/DogDoorSimulator.java
@@ -1,5 +1,8 @@
 package dougsdogdoor;
 
+import java.util.Timer;
+import java.util.TimerTask;
+
 public class DogDoorSimulator {
     public static void main(String args[]) {
         DogDoor door = new DogDoor();
@@ -8,10 +11,15 @@ public class DogDoorSimulator {
         System.out.println("Fido barks to go outside");
         remote.pressButton();
         System.out.println("\nFido has gone out");
-        remote.pressButton();
+        final Timer timer = new Timer();
+        timer.schedule(new TimerTask() {
+            @Override
+            public void run() {
+                System.out.println("Fido Barks!!");
+                remote.pressButton();
+                System.out.println("\nFido's back inside");
+            }
+        },10000);
         System.out.println("\nFido's all done");
-        remote.pressButton();
-        System.out.println("\nFido's back inside");
-        remote.pressButton();
     }
 }
